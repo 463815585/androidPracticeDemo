40955be1615062f55ce8fe74fc0f3b7604a77865
蒋大德 <463815585@qq.com>
2024-05-16 01:31:11 +0800

    使用constrainSet实现constraintLayout关键帧动画效果，注意：只能改变原来元素的大小和位置，无法改变内容，因为使用了wrap_content根据内容控制大小，所以最终效果大小也没有变化。

--- app/src/main/java/com/JDD/demo/MainActivity.kt
+++ app/src/main/java/com/JDD/demo/MainActivity.kt
@@ -1,12 +1,19 @@
  package com.JDD.demo
 
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.transition.TransitionManager
 
-class MainActivity : AppCompatActivity() {
+ class MainActivity : AppCompatActivity() {
+     private lateinit var btn1 : TextView;
+     private lateinit var constraintLayout : ConstraintLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +23,19 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        btn1 = findViewById(R.id.btn_1)
+        constraintLayout = findViewById(R.id.main)
+        btn1.setOnClickListener {
+            animateToKeyframeTwo()
+            Toast.makeText(this@MainActivity, "点击了1", Toast.LENGTH_SHORT).show()
+        }
     }
+
+    private fun animateToKeyframeTwo() {
+        val constraintSet = ConstraintSet()
+        constraintSet.load(this, R.layout.activity_main_two)
+        TransitionManager.beginDelayedTransition(constraintLayout)
+        constraintSet.applyTo(constraintLayout)
+    }
+
 }
